dc9a72ccb8628bea20dac4cca3d438a5508510c0
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-13 00:19:42 +0530

    local_server/config.php

--- local_server/config.php
+++ local_server/config.php
@@ -3,7 +3,7 @@ ob_start();
 session_start();
 date_default_timezone_set('Asia/Kolkata');
 // primary server path
-$base_url="http://192.168.1.5/savsoftquiz_micro/local_server/";
+$base_url="http://local.rocket.co.in:8081/test1/savsoftquiz_micro/local_server/";
 $passCode="1234";
 $userLogin="userData-encrypted-22022019002301.txt";
 $quizData="quizData-encrypted-22022019002301.txt";
